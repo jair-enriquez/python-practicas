382c1378f9270a31c1f352e1e9567fed6688f23f
Jair Pedro Enriquez Martinez <emartinezjp@uss.edu.pe>
2026-07-03 18:07:18 -0500

    Delete fundamentos/08-Calculadora.py

--- fundamentos/08-Calculadora.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("Ingresa el primer número: "))
-n2 = int(input("Ingresa el segundo número: "))   
-
-print ("#############")
-print ("Jair Enriquez Martinez")
-print ("El resultado de la suma es: ", n1 + n2)
-print ("El resultado de la resta es: ", n1 - n2)
-print ("El resultado de la multiplicación es: ", n1 * n2)
-print ("El resultado de la división es: ", n1 / n2) 
